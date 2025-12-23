a0f4aab6679d0f57a45124cc410d8969b87228f0
Bikal Magar <mbikal85@gmail.com>
2025-12-23 09:47:43 +0545

    Fix subprocess call and improve test output

--- test_pki_tool.py
+++ test_pki_tool.py
@@ -1,14 +1,18 @@
 import subprocess
+
 def test_pki_output():
-  result = subprocess.run(
-    ["python", "pki_tool.py"],
-    capture_output = True,
-    text = True
-  )
-  output = result.stdout
-  assert "Public key:" in output, "Missing public key"
-  assert "Private key:" in output, ""Missing private key"
-  print("PKI tool tesst passed!")
+    result = subprocess.run(
+        ["python3", "pki_tool.py"],
+        capture_output=True,
+        text=True
+    )
+
+    output = result.stdout
+
+    assert "Public key:" in output, "Missing public key"
+    assert "Private key:" in output, "Missing private key"
+
+    print("PKI tool test passed!")
 
 if __name__ == "__main__":
     test_pki_output()
